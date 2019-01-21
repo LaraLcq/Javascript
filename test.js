d731a28bcd8ea3a4fb50a1bbe5cec25d888075c2
laralcq <laraloicq@gmail.com>
2019-01-21 10:04:56 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,4 @@
+console.log("It works !!!");
+const readlineSync = require("readline-sync");
+let userName = readlineSync.question('Can you give me your name please?');
+console.log("Hello " + userName);

f036d6a1c520d25dc9142f7c8b6616de5f2d6c6c
laralcq <laraloicq@gmail.com>
2019-01-21 10:41:37 +0100

    fin ex4

---
+++ exercice1-4.js
@@ -0,0 +1,5 @@
+const readlineSync = require("readline-sync");
+let name = readlineSync.question ("What's your name ?");
+let firstname = readlineSync.question ("Wath's your firstname ?");
+let city = readlineSync.question ("Where do you live ?");
+console.log ("You're name is " + name + firstname + " and you live in " + city);

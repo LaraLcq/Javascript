1a3a762e0a5f848ed94a0660d4251e90e0186876
laralcq <laraloicq@gmail.com>
2019-01-22 12:05:18 +0100

    ex3-2

---
+++ ex3-2.js
@@ -0,0 +1,14 @@
+let values = [1, 2, 3, 4, 5];
+let count = values.reduce((previous, current) => current += previous);
+let avg = count / values.length;
+
+console.log(avg);
+
+
+let arr = [100, 101, 102];
+let sum = arr.reduce((previous, current) => current += previous);
+let avgi = sum / arr.length;
+
+console.log(avgi);
+
+

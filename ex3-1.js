67ea12e41a464af98288c369e2f496f65172f7e2
laralcq <laraloicq@gmail.com>
2019-01-22 11:55:36 +0100

    ex 3-1

---
+++ ex3-1.js
@@ -0,0 +1,24 @@
+let arr = [1, 2, 3, 4, 5]; // le tableau
+var cpt=0; // le compteur
+
+for (var i=0; i<arr.length; i++) {
+	cpt += arr[i]
+}
+
+console.log(cpt);
+
+
+let arn = [100, 101, 102]; // le tableau
+var cpt =0; // le compteur
+
+for (var i=0; i<arn.length; i++) {
+	cpt += arn[i]
+}
+
+console.log(cpt);
+
+
+
+
+ 
+

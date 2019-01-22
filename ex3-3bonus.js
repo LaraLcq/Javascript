2ec03784ec56be7c9d1afa5be3d9e28902c76acb
laralcq <laraloicq@gmail.com>
2019-01-22 15:27:06 +0100

    3 bonus

---
+++ ex3-3bonus.js
@@ -0,0 +1,12 @@
+/* var values = [1, 2, 3, 4, 5];
+var newArr = values.slice();
+
+values.push(newArr);
+
+console.log(newArr); */
+
+var values = [1, 2, 3, 4, 5];
+var total = values.push;
+
+console.log(values); // ["plongée", "baseball", "football", "tennis"]
+console.log(total);  // 4

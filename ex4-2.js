1654caba8a54f7518f9a1458b68f8b75b246e97e
laralcq <laraloicq@gmail.com>
2019-01-22 16:34:53 +0100

    fin ex4-2

---
+++ ex4-2.js
@@ -0,0 +1,11 @@
+
+let rand10 =()=>{
+
+	var nb = (Math.random() * (10) + 1 ); // random donne un nombre entre 0 et 1 > il donnera 0 à 9,9 donc (10) + 1 pour avoir 10 et de 
+	return Math.floor(nb);
+}
+	
+console.log(rand10());
+
+
+// renvoyer une valeur avec une fonction 

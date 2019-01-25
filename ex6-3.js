ccacd73b74f55aa5329d246701ae52936187eb1a
laralcq <laraloicq@gmail.com>
2019-01-25 09:01:01 +0100

    mise à jour

--- ex6-3.js
+++ ex6-3.js
@@ -7,20 +7,35 @@ class Rectangle {
 		this.length =length;
 	}
 	collides(otherRectangle){
-		if(this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width &&
+		return(this.topLeftXPos < otherRectangle.topLeftXPos + otherRectangle.width &&
 	   this.topLeftXPos + this.width > otherRectangle.topLeftXPos &&
 	   this.topLeftYPos < otherRectangle.topLeftYPos + otherRectangle.length &&
 	   this.length + this.topLeftYPos > otherRectangle.topLeftYPos);
-
-	   return Math.floor(Math.random() * 1000
-
-		}
+	}
 	/* var topLeftXPos = Math.floor(Math.random() * 1000;
     var topLeftYPos = Math.floor(Math.random() * 1000; */
 	
 }
 
+// console.log(rect1.collides(rect2)); 
 
-let rect1 = new Rectangle ();
-let rect2 = new Rectangle ();
-console.log(rect1.collides(rect2)); 
+function thousandRect() {
+let myObject = {
+	rect :[]
+}
+	for (i = 0; i <= 999; i++){
+		myObject.rect[i] = new Rectangle (Math.floor(Math.random() * 1000),Math.floor(Math.random() * 1000),Math.floor(Math.random() * 1000),Math.floor(Math.random() * 1000));
+	console.log(i);
+	}
+
+//collapse test
+	for (i = 0; i <= 999; i++){
+		for (j = 0; j <= 999; j++){
+			if(i != j){
+				console.log("rect: " + i + " vs: " + j);
+				console.log(myObject.rect[i].collides(myObject.rect[j]));
+			}
+		}
+	}
+}
+thousandRect();

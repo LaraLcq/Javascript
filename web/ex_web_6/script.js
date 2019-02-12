e2a59266d1099fd1dd2bae9aade9c55cc65d0f7a
laralcq <laraloicq@gmail.com>
2019-02-12 10:49:14 +0100

    ajout ex6

---
+++ web/ex_web_6/script.js
@@ -0,0 +1,47 @@
+
+// import some polyfill to ensure everything works OK
+import "babel-polyfill"
+
+// import bootstrap's javascript part
+import 'bootstrap';
+
+// import the style
+import "./style.scss";
+
+/*
+  Put the JavaScript code you want below.
+*/
+
+import cat1 from './_shared/img/1.jpg'
+import cat2 from './_shared/img/2.jpg'
+import cat3 from './_shared/img/3.jpg'
+import cat4 from './_shared/img/4.jpg'
+import cat5 from './_shared/img/5.jpg'
+import cat6 from './_shared/img/6.jpg'
+import cat7 from './_shared/img/7.jpg'
+
+let arr=[cat1,cat2,cat3,cat4,cat5,cat6,cat7]
+let i=0;
+setInterval(function(){
+	if (i >=arr.length){
+		i=0;
+	}
+	console.log(i)
+	document.querySelector("#cats").src=arr[i] // changer la source
+	i++ // incrémenter de 1 
+}, 10000)
+
+function shuffle(array){
+	var m = array.length, a, i;
+
+	while (m) {
+		i = Math.floor(Math.random() * m--);
+		a = array[m];
+		array[m] = array[i];
+		array[i] = a;
+	}
+
+	return array;
+}
+
+document.getElementbyid("shuffle").addEventListener("click",shuffle(arr))
